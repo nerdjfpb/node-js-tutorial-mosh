203c12e5100efbd095093fdeb8e44fbb41d7afc8
Muhammad Ali <nerdjfpb@gmail.com>
2019-06-25 15:30:08 +0600

    input validations

--- index.js
+++ index.js
@@ -25,12 +25,18 @@ app.get('/api/courses/:id', (req,res) => {
 
 app.post('/api/courses', (req,res) => {
 	
+	if(!req.body.name || req.body.name.length <3) {
+		res.status(400).send('Name is required and should be minimum 3 characters');
+		return;
+	}
+
 	const course = {
 		id: courses.length +1,
 		name: req.body.name
 	};
 	courses.push(course);
 	res.send(course);
+
 });
 
 
