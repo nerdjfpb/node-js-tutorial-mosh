d892f62e9d61c417e97263694e05dd9345970927
Muhammad Ali <nerdjfpb@gmail.com>
2019-06-25 15:58:02 +0600

    Updating data using put with joi validation

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const courses = [
 	{id: 3, name: 'course3'},
 ];
 
+
 app.get('/', (req, res) =>{
 	res.send('Hello World New Game');
 });
@@ -26,13 +27,9 @@ app.get('/api/courses/:id', (req,res) => {
 
 app.post('/api/courses', (req,res) => {
 
-	const schema = {
-		name : Joi.string().min(3).required()
-	};
-	
-	const result = Joi.validate(req.body, schema);
-	if(result.error) {
-		res.status(400).send(result.error.details[0].message);
+	const { error } = validateCourse(req.body);	
+	if(error) {
+		res.status(400).send(error.details[0].message);
 		return;
 	}
 
@@ -44,6 +41,36 @@ app.post('/api/courses', (req,res) => {
 	res.send(course);
 
 });
+
+
+app.put('/api/courses/:id',(req,res) => {
+
+	const course = courses.find(c => c.id === parseInt(req.params.id));
+	if (!course) res.status(400).send('The Course with the given Id was not found');
+
+	const { error } = validateCourse(req.body);	
+	if(error) {
+		res.status(400).send(error.details[0].message);
+		return;
+	}
+
+	course.name = req.body.name;
+	res.send(course);
+
+});
+
+
+
+function validateCourse(course){
+	
+	const schema = {
+		name : Joi.string().min(3).required()
+	};
+
+	return Joi.validate(course, schema);
+}
+
+
 
 
 //Port
