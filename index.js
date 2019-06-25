bd590601dd7b50b1ab383d32e5c6c0147fa9f9e1
Muhammad Ali <nerdjfpb@gmail.com>
2019-06-25 15:36:22 +0600

    joi added for input validation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -24,9 +25,14 @@ app.get('/api/courses/:id', (req,res) => {
 });
 
 app.post('/api/courses', (req,res) => {
+
+	const schema = {
+		name : Joi.string().min(3).required()
+	};
 	
-	if(!req.body.name || req.body.name.length <3) {
-		res.status(400).send('Name is required and should be minimum 3 characters');
+	const result = Joi.validate(req.body, schema);
+	if(result.error) {
+		res.status(400).send(result.error.details[0].message);
 		return;
 	}
 
